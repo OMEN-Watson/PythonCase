8797717172c7866f94235ca103d5c8b581697238
u7568823 <u7568823@anu.edu.au>
2024-06-01 15:44:40 +1000

    done with p5

--- comp1730/practice/test.py
+++ comp1730/practice/test.py
@@ -11,15 +11,20 @@
 # print(d)
 aArray=[1,2,3]
 bArray=['1',[1,2],'d']
-print(aArray)
-print(bArray+aArray)
-print(isinstance(aArray,list))
-print(isinstance(aArray,tuple))
-print(type(aArray)==list)
+# print(aArray)
+# print(bArray+aArray)
+# print(isinstance(aArray,list))
+# print(isinstance(aArray,tuple))
+# print(type(aArray)==list)
 
 def smallest_non_negative(number_list):
   return 0
 
 # assert smallest_non_negative(aArray)==1,'yes'
 # print(bArray[0]%5)
-print(bArray[1]%5)
+# print(bArray[1]%5)
+
+dictA={1:'1',2:'2'}
+dictB={2:'2',1:'1'}
+print(dictA==dictB)
+print(id(dictA)==id(dictB))
